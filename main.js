419ce3cb0ce752dd3dae3c79c82f5ab1dbd44899
Kraigo <kraigo@live.com>
2019-07-16 02:41:41 +0300

    updating

--- main.js
+++
@@ -1,9 +0,0 @@
-
-
-document.addEventListener('DOMContentLoaded', function(){
-
-    // const lang = navigator.language || navigator.userLanguage; 
-    // return lang.substring(0, 2);
-    const birthday = Math.floor((Date.now() - new Date(1990, 05, 22)) / 1000 / 60 / 60 / 24 / 365);
-    document.getElementById('birthday').innerText = birthday;
-})
---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const lang = (navigator.language || navigator.userLanguage).substring(0, 2); 
+    document.body.classList.add('lang-' + lang);
+
+
+    const birthday = Math.floor((Date.now() - new Date(1990, 05, 22)) / 1000 / 60 / 60 / 24 / 365);
+    document.getElementById('js-birthday').innerText = birthday;
+    
+    document.getElementById('js-current-year').innerText = new Date().getFullYear();
+
+    
+    const elements = document.querySelectorAll('.balloon, .section-title, .radial, .timeline');			   
+    new ShowByScroll(elements, {});
+    
+})
